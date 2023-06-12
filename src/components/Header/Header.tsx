ce9cacaf9fff2d2382c669a9bbbe9ee510ec75c3
Dian Azizah <nive54@gmail.com>
2023-06-12 19:50:13 +0700

    update header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -8,31 +8,39 @@ function Header() {
 
   return (
     <AnimOnAppear>
-    <header className="w-full px-4 py-6 font-mono flex items-center justify-between max-w-7xl mx-auto">
-      <Link to={'/'} className="text-glow react-effect text-[24px]">
-        Home
-      </Link>
-      <nav>
-        <ul className="flex items-center">
-          {navMenu.map(({ label, path }) => (
-            <li key={path} className="sm:mr-2">
-              <Link to={path} className="px-1 sm:px-2 code-effect text-md sm:text-[20px] tracking-wide text-glow">
-                {label}
-              </Link>
+      <header 
+        className="w-full px-4 py-6 font-mono flex items-center justify-between max-w-7xl mx-auto"
+      >
+        <Link 
+          to={'/'} 
+          className="text-glow sm:react-effect"
+        >
+          Home
+        </Link>
+        <nav>
+          <ul className="flex items-center">
+            {navMenu.map(({ label, path }) => (
+              <li key={path} className="sm:mr-2">
+                <Link 
+                  to={path} 
+                  className="px-1 sm:px-2 code-effect text-sm sm:text-lg tracking-wide text-glow"
+                >
+                  {label}
+                </Link>
+              </li>
+            ))}
+            <li className="">
+              <button
+                title={language === 'id' ? 'Indonesia' : 'English'}
+                onClick={toggleLanguage}
+                className="code-effect text-[20px] sm:px-2 px-0 text-glow"
+              >
+                {language === 'id' ? 'ID' : 'EN'}
+              </button>
             </li>
-          ))}
-          <li className="">
-            <button
-              title={language === 'id' ? 'Indonesia' : 'English'}
-              onClick={toggleLanguage}
-              className="code-effect text-[20px] sm:px-2 px-0 text-glow"
-            >
-              {language === 'id' ? 'ID' : 'EN'}
-            </button>
-          </li>
-        </ul>
-      </nav>
-    </header>
+          </ul>
+        </nav>
+      </header>
     </AnimOnAppear>
   );
 }
