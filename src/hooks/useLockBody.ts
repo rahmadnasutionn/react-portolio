13b94e5426cbdc7dc93df1f3ebb0656a3d2896c9
Dian Azizah <dianazizah3@gmail.com>
2023-07-25 09:05:27 -0700

    add useLockBody for mobileNav

---
+++ src/hooks/useLockBody.ts
@@ -0,0 +1,14 @@
+
+
+
+function useLockBody() {
+  useLayoutEffect((): (() => void) => {
+    const originalStyle: string = window.getComputedStyle(
+      document.body,
+    ).overflow;
+    document.body.style.overflow = 'hidden';
+    return () => (document.body.style.overflow = originalStyle)
+  }, []);
+}
+
+export default useLockBody
