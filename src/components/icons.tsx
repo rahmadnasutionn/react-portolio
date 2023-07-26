2afb9a977563a72da2b8c11ba9112afaba53b4fb
Dian Azizah <dianazizah3@gmail.com>
2023-07-26 03:25:28 -0700

    remove icons menu, close change with custom animation burger menu

--- src/components/icons.tsx
+++ src/components/icons.tsx
@@ -52,37 +52,3 @@ export const GithubIcon = ({ className }: { className?: string }) => (
     />
   </svg>
 )
-
-export const Menu = () => (
-  <svg
-    xmlns="http://www.w3.org/2000/svg"
-    className="h-6 w-6"
-    fill="none"
-    viewBox="0 0 24 24"
-    stroke="currentColor"
-  >
-    <path
-      strokeLinecap="round"
-      strokeLinejoin="round"
-      strokeWidth="2"
-      d="M4 6h16M4 12h16m-7 6h7"
-    />
-  </svg>
-);
-
-export const Close = () => (
-  <svg
-    xmlns="http://www.w3.org/2000/svg"
-    className="h-6 w-6"
-    fill="none"
-    viewBox="0 0 24 24"
-    stroke="currentColor"
-  >
-    <path
-      strokeLinecap="round"
-      strokeLinejoin="round"
-      strokeWidth={2}
-      d="M6 18L18 6M6 6l12 12"
-    />
-  </svg>
-);
