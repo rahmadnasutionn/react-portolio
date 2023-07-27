57de4be7e407dab84d0b772e926e8407325e003f
Dian Azizah <dianazizah3@gmail.com>
2023-07-27 08:48:12 -0700

    import * img

---
+++ src/assets/index.ts
@@ -0,0 +1,23 @@
+import react from './tools/reactjs.png';
+import nodeJs from './tools/nodejs.png';
+import mongoDb from './tools/mongodb.png';
+import redux from './tools/redux.png';
+import typescript from './tools/typescript.png';
+import tailwind from './tools/tailwind.png';
+import html from './tools/html.png';
+import javascript from './tools/javascript.png';
+import css from './tools/css.png';
+import git from './tools/git.png';
+
+export {
+  react,
+  html,
+  javascript,
+  css,
+  git,
+  redux,
+  nodeJs,
+  mongoDb,
+  typescript,
+  tailwind,
+};
