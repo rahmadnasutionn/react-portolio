2ba6b6dd403a6e25e528433a0c318a2d28173881
Dian Azizah <dianazizah3@gmail.com>
2023-08-20 06:18:01 -0700

    add icon youtube

--- src/components/icons.tsx
+++ src/components/icons.tsx
@@ -37,12 +37,12 @@ export const GitHub = ({ width, height, className }: { width?: number, height?: 
 
 export const GithubIcon = ({ className }: { className?: string }) => (
     <svg
-    xmlns="http://www.w3.org/2000/svg"
-    className={`${className || 'h-6 w-6'}`}
-    viewBox="0 0 1024 1024"
-    fill="none"
-    style={{ padding: '2px' }}
-  >
+      xmlns="http://www.w3.org/2000/svg"
+      className={`${className || 'h-6 w-6'}`}
+      viewBox="0 0 1024 1024"
+      fill="none"
+      style={{ padding: '2px' }}
+    >
     <path
       fillRule="evenodd"
       clipRule="evenodd"
@@ -50,5 +50,17 @@ export const GithubIcon = ({ className }: { className?: string }) => (
       transform="scale(64)"
       fill="currentColor"
     />
+  </svg>
+)
+
+export const YoutubeIcon = ({ className }: { className?: string }) => (
+  <svg className={className || 'w-6 h-6'} version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" 
+	 viewBox="0 0 461.001 461.001" xmlSpace="preserve">
+    <g>
+	    <path fill="currentColor" d="M365.257,67.393H95.744C42.866,67.393,0,110.259,0,163.137v134.728
+		    c0,52.878,42.866,95.744,95.744,95.744h269.513c52.878,0,95.744-42.866,95.744-95.744V163.137
+		    C461.001,110.259,418.135,67.393,365.257,67.393z M300.506,237.056l-126.06,60.123c-3.359,1.602-7.239-0.847-7.239-4.568V168.607
+		    c0-3.774,3.982-6.22,7.348-4.514l126.06,63.881C304.363,229.873,304.298,235.248,300.506,237.056z"/>
+    </g>
   </svg>
 )
